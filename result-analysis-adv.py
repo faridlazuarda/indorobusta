be2b2e4207e3a31c85df4dcda99a5b7b9da949d8
faridlazuarda <faridlazuarda@gmail.com>
2022-06-12 02:38:38 +0000

    add result analysis

--- result-analysis-adv.py
+++ result-analysis-adv.py
@@ -5,8 +5,8 @@ from icecream import ic
 # exp_dataset.to_csv(os.getcwd() + r'/result/seed'+str(seed)+"/"+str(dataset)+"/"+str(exp_name)+".csv", index=False)
 
 # Get the list of all files and directories
-def result_summary(seed, dataset):
-    path = str(os.getcwd()) + "/result/seed" + str(seed) + "/" + str(dataset) + "/"
+def result_summary(seed):
+    path = str(os.getcwd()) + "/adversarial-training/result/seed" + str(seed) + "/"
     dir_list = os.listdir(path)
     result = pd.DataFrame()
     
@@ -25,9 +25,14 @@ def result_summary(seed, dataset):
                     "perturb_ratio": parsed_filename[4],
                     "before_attack_acc": df_first_row["before_attack_acc"],
                     "after_attack_acc": df_first_row["after_attack_acc"],
-                    "delta_acc": (df_first_row["before_attack_acc"] - df_first_row["after_attack_acc"]),
+                    "delta_acc": df_first_row["delta_acc"],
                     "avg_semantic_sim": df_first_row["avg_semantic_sim"],
-                    "avg_running_time(s)": df_first_row["avg_running_time(s)"]
+                    "avg_running_time(s)": df_first_row["avg_running_time(s)"],
+                    "adv_training": df_first_row["adv_training"],
+                    "acc_improvement_on_adv_train": (df_first_row["adv_training"] - df_first_row["after_attack_acc"]),
+                    "acc_adv_training_on_orig": df_first_row["acc_adv_training_on_orig"],
+                    "orig_acc_vs_adv": (df_first_row["acc_adv_training_on_orig"] - df_first_row["before_attack_acc"]),
+                    "best_epoch": df_first_row["best_epoch"]
                 }
             else:
                 record = {
@@ -41,60 +46,30 @@ def result_summary(seed, dataset):
                     "after_attack_acc": df_first_row["after_attack_acc"],
                     "delta_acc": (df_first_row["before_attack_acc"] - df_first_row["after_attack_acc"]),
                     "avg_semantic_sim": df_first_row["avg_semantic_sim"],
-                    "avg_running_time(s)": df_first_row["avg_running_time(s)"]
+                    "avg_running_time(s)": df_first_row["avg_running_time(s)"],
+                    "adv_training": df_first_row["adv_training"],
+                    "acc_improvement_on_adv_train": (df_first_row["adv_training"] - df_first_row["after_attack_acc"]),
+                    "acc_adv_training_on_orig": df_first_row["acc_adv_training_on_orig"],
+                    "orig_acc_vs_adv": (df_first_row["acc_adv_training_on_orig"] - df_first_row["before_attack_acc"]),
+                    "best_epoch": df_first_row["best_epoch"]
                 }
 
             df_dictionary = pd.DataFrame([record])
             result = pd.concat([result, df_dictionary], ignore_index=True)
     ic(seed)
-    ic(dataset)
-    ic(result)
     # result.to_csv("result-analysis/" + str() + ".csv")
     result = result.sort_values(by=['model_target', 'downstream_task', 'codemix_lang', 'perturb_ratio'])
-    result.to_csv(os.getcwd() + r'/result-analysis/seed'+str(seed)+"-"+str(dataset)+".csv", index=False)
+    result.to_csv(os.getcwd() + r'/result-analysis/adv-training/seed'+str(seed)+".csv", index=False)
 
 if __name__ == "__main__":
     result_summary(
-        seed=26092020,
-        dataset="valid"
+        seed=26092020
     )
     
     result_summary(
-        seed=24032022,
-        dataset="valid"
+        seed=24032022
     )
     
     result_summary(
-        seed=42,
-        dataset="valid"
-    )
-    
-    result_summary(
-        seed=26092020,
-        dataset="train"
-    )
-    
-    result_summary(
-        seed=24032022,
-        dataset="train"
-    )
-    
-    result_summary(
-        seed=42,
-        dataset="train"
-    )
-    
-    result_summary(
-        seed=26092020,
-        dataset="test"
-    )
-    
-    result_summary(
-        seed=24032022,
-        dataset="test"
-    )
-    
-    result_summary(
-        seed=42,
-        dataset="test"
+        seed=42
     )
